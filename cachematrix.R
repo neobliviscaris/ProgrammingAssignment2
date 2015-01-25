8f435540c94722517566d66bdcabbe4c0a94f337
neobliviscaris <carlos.morales.duran@me.com>
2015-01-25 13:38:39 -0800

    Week 3, Programming Assignment 2. makeCacheMatrix() and cacheSolve() functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+## Functions to cache the inverse of a matrix. Since matrix inversion is usually
+## a costly computations, there might be benefits to caching the results of inversion 
+## rather than compute it repeatedly. 
+## 
+## This file contains functions that provide a cache layer on top a regular 
+## matrix and a function to make this additional layer transparent.
 
-## Write a short comment describing this function
-
+## Constructs a matrix wrapper that maintains a cached result of inverting the matrix.
+## 'x' The wrapped matrix whose inversion result will be cached
 makeCacheMatrix <- function(x = matrix()) {
+    inverse <- NULL
 
+    ## Sets the underlying matrix, destroys any current inversion result.
+    set <- function(newX) {
+    	x <<- newX
+    	inverse <<- NULL
+    }
+
+    ## Returns the underlying (raw) matrix.
+    get <- function() x
+
+    ## Sets the inverse cached by this CacheMatrix.
+    setinverse <- function(matrixInverse) {
+    	inverse <<- matrixInverse
+    }
+
+    ## Returns the currently cached inverse result.
+    getinverse <- function() inverse
+
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Returns the inverse of a matrix, preventing multiple calculations
+## of the inverse if a previous result has already been computed
+## 'x' The matrix whose inverse needs to be computed
+## '...' Additional parameters for the inverse() library function
 cacheSolve <- function(x, ...) {
+        inverse = x$getinverse()
+
+        if (!is.null(inverse)) {
+        	return inverse
+        }
+
+        originalMatrix <- x$get()
+        inverse <- solve(originalMatrix, ...)
+        x.setinverse(inverse)
+
         ## Return a matrix that is the inverse of 'x'
+        inverse
 }
